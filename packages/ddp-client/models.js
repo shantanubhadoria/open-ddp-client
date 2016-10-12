6dd899095741472a71f5e6d30a5da8ee8fdd1e9f
Shantanu Bhadoria <shantanu@cpan.org>
2016-10-12 13:58:30 +0800

    Moving to yeoman based ts

--- packages/ddp-client/models.js
+++
@@ -1,5 +0,0 @@
-var DDPMessageType;
-(function (DDPMessageType) {
-    DDPMessageType[DDPMessageType['connect'] = 0] = 'connect';
-    DDPMessageType[DDPMessageType['message'] = 1] = 'message';
-})(DDPMessageType || (DDPMessageType = {}));
--- typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/es6-shim/index.d.ts" />
-/// <reference path="globals/mocha/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/requirejs/index.d.ts" />
-/// <reference path="globals/ws/index.d.ts" />
