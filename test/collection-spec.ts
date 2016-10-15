2956c3ced2a359e0bfbdae577a2cd6d3c7a048d3
Shantanu Bhadoria <shantanu.bhadoria@gmail.com>
2016-10-15 17:54:43 +0800

    fixed lint errors

--- test/collection-spec.ts
+++ test/collection-spec.ts
@@ -40,7 +40,14 @@ describe("Collection", () => {
       let subscription = testCollection.collection.subscribe((collection: Array<IDDPDocument>) => {
         // Skip initial empty value
         if (collection.length > 0) {
-          expect(collection).to.eql([{"_id": "testId1", "testField": "testField1"}]);
+          expect(collection).to.eql(
+            [
+              {
+                _id: "testId1",
+                testField: "testField1"
+              }
+            ]
+          );
           done();
         }
       });
@@ -52,7 +59,14 @@ describe("Collection", () => {
       let subscription = testCollection.collection.subscribe((collection: IDDPDocument[]) => {
         // Skip initial empty value
         if (collection.length > 0) {
-          expect(collection).to.eql([{"_id": "testId1", "testField": "testField1"}]);
+          expect(collection).to.eql(
+            [
+              {
+                _id: "testId1",
+                testField: "testField1"
+              }
+            ]
+          );
           done();
         }
       });
@@ -77,7 +91,10 @@ describe("Collection", () => {
         // Skip initial empty value
         if (collection.length > 1) {
           expect(collection).to.eql([
-            {"_id": "testId1", "testField": "testField1"},
+            {
+              _id: "testId1",
+              testField: "testField1"
+            },
             {
               _id: "testId2",
               testField: "testField2",
@@ -151,7 +168,14 @@ describe("Collection", () => {
       };
       let subscription = testCollection.collection.subscribe((collection: IDDPDocument[]) => {
         if (collection.length < 2) {
-          expect(collection).to.eql([{_id:"testId1", testField:"testField1"},]);
+          expect(collection).to.eql(
+            [
+              {
+                _id: "testId1",
+                testField: "testField1"
+              },
+            ]
+          );
           done();
         }
       });
