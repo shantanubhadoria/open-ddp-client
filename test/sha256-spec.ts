31cc1ae2fbd5a7cf686911d8a7c982aaaa0fed52
Shantanu Bhadoria <shantanu.bhadoria@gmail.com>
2016-10-15 18:26:52 +0800

    fixed lint errors

--- test/sha256-spec.ts
+++ test/sha256-spec.ts
@@ -6,8 +6,8 @@ import "mocha";
 
 describe("SHA256", () => {
   it("delivers accurate hash with a known sample", () => {
-    var input = "shantanubhadoria";
-    var hash = new SHA256(input);
+    let input = "shantanubhadoria";
+    let hash = new SHA256(input);
     expect(hash.toString()).to.equal("047bdb8ec1ca1fbae3091e25857fdeb85340f93efe3e6ec2abe412e43db69cd2");
   });
 });
