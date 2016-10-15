881a04ca4673c1e178dffad02c00e4b1d3641843
Shantanu Bhadoria <shantanu.bhadoria@gmail.com>
2016-10-15 18:16:54 +0800

    removing lib to pass tests

--- lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Accounts } from "./accounts";
-import { Collection } from "./collection";
-import { DDPClient } from "./ddp-client";
-import { Methods } from "./methods";
-import { SHA256 } from "./sha256";
-export { DDPClient, Collection, Methods, Accounts, SHA256 };
--- lib/random/index.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class Random {
-    static hexString(digits: number): string;
-    static randomString(charsCount: number, alphabet: string): string;
-    static choice(arrayOrString: Array<string> | string): string;
-}
